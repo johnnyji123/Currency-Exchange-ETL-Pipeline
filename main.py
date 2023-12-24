5662aad3604d997c54024ec63023c55c7800bef9
Johnny Ji <jj_johnny97@hotmail.com>
2023-12-24 12:14:19 +0000

    handled ETL failure by using try- except to catch any exceptions

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import json
 import mysql.connector
 from sqlalchemy import create_engine
 from apscheduler.schedulers.background import BlockingScheduler
+import smtplib
 
 
+# Connecting to db
 db = mysql.connector.connect(
         host = "localhost",
         user = "root",
@@ -15,16 +17,35 @@ db = mysql.connector.connect(
     
     )
 
+# Creating cursor object
 cursor = db.cursor()
 
 
 url = "https://v6.exchangerate-api.com/v6/cd86934b5b13777e2411cea1/latest/GBP"
 
-response = requests.get(url)
-data = json.loads(response.text)
-df = pd.json_normalize(data['conversion_rates'])
-df = pl.from_pandas(df)
 
+
+def api_call(url):
+    try: 
+        response = requests.get(url)
+        data = json.loads(response.text)
+        df = pd.json_normalize(data['conversion_rates'])
+        df = pl.from_pandas(df)
+        
+        return df
+        
+    except Exception as e:
+        from_email = "example@gmail.com"
+        to_email = "example@hotmail.com"
+        message = "couldn't fetch data", e
+        
+        smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp_server.starttls()
+        smtp_server.login(from_email, "app password")
+        smtp_server.sendmail(from_email, to_email, message)
+    
+
+df = api_call(url)
 
 # UK GBP
 # China CNY
@@ -64,20 +85,19 @@ engine = create_engine(connection_string)
 
 #add_exchange_rate = df.to_sql('currency_exchange_rate', engine, if_exists = 'append', index = False)
 
-def update_database(df):
+def update_database():
     for index, row in df.iterrows():
         query =  "UPDATE currency_exchange_rate SET GBP = %s, CNY = %s, USD = %s, KRW = %s, JPY = %s, THB = %s, HKD = %s, CAD = %s, EUR =%s WHERE GBP = 1"
         cursor.execute(query, (*row, ))
+        db.commit()
      
 
-def test():
-    print("hi")
-    
-scheduler = BlockingScheduler()     
-scheduler.add_job(test, "interval", seconds = 2)
-scheduler.start()
 
-        
+#scheduler = BlockingScheduler()     
+#scheduler.add_job(update_database, "cron", hour = 17,  minute = 30)
+#scheduler.start()
+
+                
 
 
     
